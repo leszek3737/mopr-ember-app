013c301f6816baa61ad20a575bd88811cad77be1
Unknown <leszek@lubcon.org>
2018-08-23 20:09:02 +0200

    Add view first step

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    sassLint: {
+      configPath: '.sass-lint.yml',
+      shouldThrowExceptions: true,
+      shouldLog: true
+    }
     // Add options here
   });
 
